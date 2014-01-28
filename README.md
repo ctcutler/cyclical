10cddafa35abecb535c108c76614a41ecc49f0cc
ctcutler <ctcutler@gmail.com>
2014-01-27 22:11:23 -0500

    Add some use cases to the readme, add cycle tips, reorganize the code a bit.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-cyclical
-========
+Cyclicalendar
+=============
+
+Use Cases
+* remind myself to do something every N hours/days/weeks/months/years
+* see things that are due now
+* see things that are due soon
+* see all things in calendar
+* mark something done (reset it)
+* add something
+* edit something
+* remove something
+* adjust where something is in its cycle
+
--- js/cyclical.js
+++ js/cyclical.js
@@ -11,12 +11,6 @@ var CYCLES = [
   { total: 86400, elapsed: 86400 * .99 },
 ];
 
-function getRadius(i) {
-  var cycleCount = CYCLES.length;
-  var radiusIncrement = (MAX_RADIUS - MIN_RADIUS) / cycleCount;
-  return ((i + 1) * radiusIncrement) + MIN_RADIUS;
-}
-
 /**
  * We need to get coordinates for a point on a circle with the
  * given radius where the angle corresponds to the given ratio.  
@@ -27,7 +21,7 @@ function getRadius(i) {
  * c) sin/cos assume a coordinate space where y values are positive above the
  *    x-axis, SVG assumes the opposite
  */
-function makeRelativeEnd(radius, ratio, isX) {
+function makeRelativeEndPoint(radius, ratio, isX) {
   // convert ratio into radians (0 => 0 . . . 1 => 2PI)
   var radians = ratio * 2 * Math.PI;
 
@@ -51,18 +45,36 @@ function makeRelativeEnd(radius, ratio, isX) {
   return coord;
 }
 
-function makePathData(d, i) {
+function getElapsedRatio(d) {
+  return d.elapsed/d.total;
+}
+
+function getStartY(i) {
+  return CENTER_Y - getRadius(i);
+}
+
+function getStartX(i) {
+  return CENTER_X;
+}
+
+function getRadius(i) {
+  var cycleCount = CYCLES.length;
+  var radiusIncrement = (MAX_RADIUS - MIN_RADIUS) / cycleCount;
+  return ((i + 1) * radiusIncrement) + MIN_RADIUS;
+}
+
+function makeInitialPathData(d, i) {
   var radius = getRadius(i);
-  var startX = CENTER_X;
-  var startY = CENTER_Y - radius;
+  var startX = getStartX(i);
+  var startY = getStartY(i);
   var xRadius = radius;
   var yRadius = radius;
   var xAxisRotation = 0;
-  var elapsedRatio = d.elapsed/d.total;
+  var elapsedRatio = getElapsedRatio(d);
   var largeArc = elapsedRatio > .5 ? 1 : 0;
   var sweep = 1; // always one as long as we start from the top and go clockwise
-  var endDx = makeRelativeEnd(radius, elapsedRatio, true, startX);
-  var endDy = makeRelativeEnd(radius, elapsedRatio, false, startY);
+  var endDx = startX;
+  var endDy = startY;
   var parts = [
     "M", startX, startY,
     "a", xRadius, yRadius, 
@@ -72,27 +84,73 @@ function makePathData(d, i) {
   return parts.join(" ");
 }
 
+function makePathData(d, i) {
+  var radius = getRadius(i);
+  var startX = getStartX(i);
+  var startY = getStartY(i);
+  var xRadius = radius;
+  var yRadius = radius;
+  var xAxisRotation = 0;
+  var elapsedRatio = getElapsedRatio(d);
+  var largeArc = elapsedRatio > .5 ? 1 : 0;
+  var sweep = 1; // always one as long as we start from the top and go clockwise
+  var endDx = makeRelativeEndPoint(radius, elapsedRatio, true);
+  var endDy = makeRelativeEndPoint(radius, elapsedRatio, false);
+  var parts = [
+    "M", startX, startY,
+    "a", xRadius, yRadius, 
+      xAxisRotation, largeArc, sweep,
+      endDx, endDy
+  ];
+  return parts.join(" ");
+}
+
+function makeCycleTipCX(d, i) {
+  var relativeEndPoint = makeRelativeEndPoint(getRadius(i), getElapsedRatio(d), true);
+  return relativeEndPoint + getStartX(i); // convert to absolute
+}
+
+function makeCycleTipCY(d, i) {
+  var relativeEndPoint = makeRelativeEndPoint(getRadius(i), getElapsedRatio(d), false);
+  return relativeEndPoint + getStartY(i); // convert to absolute
+}
+
 function render() {
   svg = d3.select("#mainSvg")
     .attr("width", WIDTH)
     .attr("height", HEIGHT);
+
+  // FIXME: setting up transitions would be cool, but wow is it complicated
+  // when acting on path data. . . here is some reading material:
+  //   http://blog.visual.ly/creating-animations-and-transitions-with-d3-js/
+  //   https://github.com/mbostock/d3/wiki/Transitions#wiki-attrTween
+  //   https://github.com/mbostock/d3/wiki/Transitions#wiki-d3_interpolateObject
+  //   http://bl.ocks.org/mbostock/1098617
+  //   https://github.com/mbostock/d3/wiki/SVG-Shapes#wiki-arc
 
   var cycle = svg.selectAll(".cycle")
     .data(CYCLES);
   cycle.enter()
     .append("path")
     .attr("class", "cycle")
-    .attr("d", makePathData)
     .attr("stroke", "green")
+    .attr("d", makeInitialPathData)
     .attr("fill", "transparent");
-  /*
   cycle
-    .attr("x1", function (d) { return d.coords.x1 })
-    .attr("y1", function (d) { return d.coords.y1 })
-    .attr("x2", function (d) { return d.coords.x2 })
-    .attr("y2", function (d) { return d.coords.y2 })
-    .attr("class", function (d) { return "connection "+d.color });
+    .attr("d", makePathData);
   cycle.exit()
     .remove();
-  */
+
+  var cycleTip = svg.selectAll("cycleTip")
+    .data(CYCLES);
+  cycleTip.enter()
+    .append("circle")
+    .attr("class", "cycleTip")
+    .attr("r", "3")
+    .attr("fill", "green");
+  cycleTip
+    .attr("cy", makeCycleTipCY)
+    .attr("cx", makeCycleTipCX);
+  cycleTip.exit()
+    .remove();
 }
